cf43b826feb8bb47cf9f133d8da486d303f2ac67
iicanovic1 <73068169+iicanovic1@users.noreply.github.com>
2022-09-06 22:06:19 +0200

    promjene verzija ovisnosti

--- src/data/collections/Answer.kt
+++ src/data/collections/Answer.kt
@@ -1,15 +1,13 @@
 package ba.unsa.etf.data.collections
 
-import ba.unsa.etf.data.collections.Surah.*
 import org.bson.types.ObjectId
-import java.util.*
 
 data class Answer (
     val type: AnswerType,
-    val juz: Juz,
-    val surah: Surah,
-    val ajehMin : Int?,
-    val ajehMax : Int?,
+    val section: Section,
+    val chapter: Chapter,
+    val sentenceMin : Int?,
+    val sentenceMax : Int?,
     val date: Long,
     val mark: Int,
     val id: String = ObjectId().toString(),
@@ -23,17 +21,17 @@ data class AnswerType (val typeName : String) {
     }
 }
 
-data class Surah (var surahName : String, val numberOfAjat : Int?) {
+data class Chapter (var chapterName : String, val numberOfSentences : Int?) {
 
     override fun toString(): String {
-        return surahName
+        return chapterName
     }
 }
 
-data class Juz (val juzName : String, val juzNumber : Int,  val surahs : List<Surah>) {
+data class Section (val sectionName : String, val sectionNumber : Int, val chapters : List<Chapter>) {
 
     override fun toString(): String {
-        return juzName
+        return sectionName
     }
 
 }
